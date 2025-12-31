0865b51bcee41457d99036b9720454517d426fe4
Ömer_fqruk <omerfaruk0801@gmail.com>
2025-12-31 18:19:34 +0300

    Add files via upload

---
+++ topalama_islemi/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int s1,s2,top=0;
+    cout<<"Bir sayi giriniz: ";
+    cin>>s1;
+    cout<<"Bir sayi daha giriniz: ";
+    cin>>s2;
+    top=s1+s2;
+    cout<<"Toplam: "<<top<<endl;
+    return 0;
+}
